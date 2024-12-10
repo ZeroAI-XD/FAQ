2ca05963564c52bd6f8c725a61bf69060af6109a
赵鑫浩 <zhaoxh@sense.com.cn>
2024-12-10 23:54:17 +0800

    修改base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
+  base: '/FAQ/',
   lang: 'en-US',
 
   title: 'VuePress',
